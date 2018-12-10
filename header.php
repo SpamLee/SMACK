e54d2178ff1b3c45a0a08b0423d214122c0359c5
Lee Phillips <leephillipsqwerty@gmail.com>
2018-12-10 20:20:00 +1000

    Added header php script for use in all parts of the website

---
+++ header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="SMACK Club Website">
+    <meta name="author" content="Lee Phillips">
+    <link rel="icon" href="images/sumako.ico">
+
+    <title>Super Manga Anime Culture Kingdom</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <link href="home.css" rel="stylesheet">
+  </head>
+
+  <body>
+  
+	<!-- SMACK Page Header -->
+    <div class="container">
+      <header class="blog-header py-3">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+          <div class="col-4 text-center">
+            <a class="blog-header-logo text-dark" href="#">SMACK</a>
+          </div>
+          <div class="col-4 d-flex justify-content-end align-items-center">
+            <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+          </div>
+        </div>
+      </header>
+
+      <div class="nav-scroller py-1 mb-2">
+        <nav class="nav">
+          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="#">About Us</a>
+          <a class="nav-link" href="#">Site Recomendations</a>
+        </nav>
+      </div>
+    </div>
+  </body>
+</html>
